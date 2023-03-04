8f9d4e2603c95ccbaeab8ec7ae823a1847e3f616
alex <alex@ubuntu>
2023-03-04 12:10:07 +0000

    feat: update stage var

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpack
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { stage: "staging" }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
